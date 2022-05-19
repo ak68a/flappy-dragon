d1cbdd69c1f4f8911da52a7249fc49d9b232063c
ak68a <a@ak68a.co>
2022-05-18 22:43:19 -0400

    Add player object

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use bracket_lib::prelude::*;
 
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut Bterm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')
+        );
+    }
+}
+
 enum GameMode {
     Menu,
     Playing,
@@ -7,7 +33,7 @@ enum GameMode {
 }
 
 struct State {
-    mode: GameMode
+    mode: GameMode,
 }
 
 impl State {
